dac8db847482a35791d75962f4bd1fa399aad999
Alessandra Boccuto <aless97b@gmail.com>
2018-07-27 12:31:30 +0200

    added ChooseSize()

--- ConnectFour/src/it/unicam/cs/pa/ConnectFour/match/ChooseMatch.java
+++ ConnectFour/src/it/unicam/cs/pa/ConnectFour/match/ChooseMatch.java
@@ -3,9 +3,11 @@
  */
 package it.unicam.cs.pa.ConnectFour.match;
 
+
 import java.util.HashMap;
 import java.util.Scanner;
 
+import it.unicam.cs.pa.ConnectFour.core.Size;
 import it.unicam.cs.pa.ConnectFour.player.InteractivePlayer;
 import it.unicam.cs.pa.ConnectFour.player.Player;
 import it.unicam.cs.pa.ConnectFour.player.RandomPlayer;
@@ -14,13 +16,13 @@ import it.unicam.cs.pa.ConnectFour.ruleSet.PopOutRuleSet;
 import it.unicam.cs.pa.ConnectFour.ruleSet.RuleSet;
 
 
-
 /**
  * @author Alessandra Boccuto
  *
  */
 public class ChooseMatch {
-
+	
+	static Scanner in = new Scanner(System.in);
 	/**
 	 * @param args
 	 */
@@ -30,7 +32,7 @@ public class ChooseMatch {
 		
 		HashMap<String, Object> prop = new HashMap<>();
 		
-		prop.put("size", DefaultRuleSet.DEFAULT_SIZE);
+		prop.put("size", chooseSize());
 		prop.put("ruleset", chooseRuleset());
 		prop.put("firstPlayer", chooseFirstPlayer());
 		Match m = Match.getInstance();
@@ -39,11 +41,38 @@ public class ChooseMatch {
 		
 		}
 
+	/**
+	 * @return Size the size of the MatchField (Default size or custom size)
+	 */
+	private static Size chooseSize() {
+		System.out.println("Do  you want to use default matchfield size 6x7? (y/n) ");
+		String line = in.nextLine();
+		while (!line.equals("n")) {
+			if (line.equals("y")) return DefaultRuleSet.DEFAULT_SIZE;
+			line = in.nextLine();
+		}		
+		return new Size(chooseNumber("rows"), chooseNumber("columns"));
+	}
+
+	/**
+	 * @return number of rows or columns
+	 */
+	private static Integer chooseNumber(String name) {
+		Scanner nm =new Scanner(System.in);
+			System.out.println("Choose the number of " + name + " (bigger than 1) : ");
+			int num = nm.nextInt();
+			while (num < 1) {
+				System.out.println("Input error");
+				num = nm.nextInt();
+			}
+		return num;
+	}
+	
 	/**  Allows player to choose the player type
 	 * @return Player
 	 */
 	private static Player choosePlayerType(int n) {
-		Scanner in =new Scanner(System.in);
+//		Scanner in =new Scanner(System.in);
 		System.out.println("Choose the player "+n+" type: (1 for InteractivePlayer, 2 for RandomPlayer)");
 		int num = in.nextInt();
 		while ((num<1)||(num>2)) {
@@ -61,30 +90,29 @@ public class ChooseMatch {
 	 * @return String the player name
 	 */
 	private static String setPName() {
-		Scanner in =new Scanner(System.in);
+		Scanner nm =new Scanner(System.in);
 		System.out.println("Choose the player name");		
-		return in.nextLine();
+		return nm.nextLine();
 	}
 
 	/**Allows player to choose who is the first player
 	 * @return int first player
 	 */
 	private static int chooseFirstPlayer() {
-		Scanner in =new Scanner(System.in);
+		//Scanner in =new Scanner(System.in);
 		System.out.println("Choose first Player: (1 for Player 1, 2 for Player 2)");
 		int num = in.nextInt();
 		while ((num<1)||(num>2)) {
 			System.out.println("input error!");
 			num=in.nextInt();
 			}
-		return num;
+		return num - 1;
 	}
 
 	/**Allows player to choose a specific game ruleset
 	 * @return Ruleset 
 	 */
 	private static RuleSet chooseRuleset() {
-		Scanner in =new Scanner(System.in);
 		System.out.println("Choose the Ruleset: (1 for Default, 2 for Pop Out)");
 		int num = in.nextInt();
 		while ((num<1)||(num>2)) {
@@ -97,5 +125,6 @@ public class ChooseMatch {
 		default: return null;
 		}
 	}
+	
 }
 
